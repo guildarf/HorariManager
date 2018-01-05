109f22960b4b073ba5a2df065284ad38bca6ec7d
marcmmr <marcmontserrrat@gmail.com>
2018-01-05 02:53:35 +0100

    He fet l'spinner amb 6 graus diferents, tmb he activat el boto validar perque crei una nova activitat sempre i quant haguessim donat un nom a l'horari. Boto de neteja creat (inoperatiu)

--- app/src/main/java/montserrat/marcet/horarimanager/SubjectsActivity.java
+++ app/src/main/java/montserrat/marcet/horarimanager/SubjectsActivity.java
@@ -1,18 +1,62 @@
 package montserrat.marcet.horarimanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class SubjectsActivity extends AppCompatActivity {
+
+    Spinner sp_graus;
+    String [] graus;
+    private boolean isfirstime = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subjects);
+        sp_graus = (Spinner) findViewById(R.id.sp_graus);
+        graus = getResources().getStringArray(R.array.llista_graus);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getBaseContext(),android.R.layout.simple_spinner_item,graus);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_graus.setAdapter(adapter);
+        sp_graus.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (isfirstime){
+                    isfirstime = false;
+                }else {
+                    Toast.makeText(getApplicationContext(),"Has triat "+graus[position], Toast.LENGTH_SHORT).show();
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
 
+        final EditText mEdit = (EditText)findViewById(R.id.id_anomena_horai);
+
+        Button btn_validate = (Button) findViewById(R.id.btn_validate);
+        btn_validate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String algo_escrit = mEdit.getText().toString();
+                if (algo_escrit.equals("")){
+                    Toast.makeText(getApplicationContext(),R.string.falta_nom, Toast.LENGTH_SHORT).show();
+                }else {
+                    startActivity(new Intent(SubjectsActivity.this, CreationActivity.class));
+                }
+
+            }
+        });
 
     }
 }
