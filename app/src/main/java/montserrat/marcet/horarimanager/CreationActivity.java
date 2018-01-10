3a1a8e47c422be946ff8e4acb62b88bcc4b90029
guillem <guillemmarcet@gmail.com>
2018-01-10 20:27:32 +0100

    si no hi ha horaris guardats ho mostra en el label de la activitat choice

--- app/src/main/java/montserrat/marcet/horarimanager/CreationActivity.java
+++ app/src/main/java/montserrat/marcet/horarimanager/CreationActivity.java
@@ -2,6 +2,7 @@ package montserrat.marcet.horarimanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class CreationActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class CreationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creation);
+
     }
 }
